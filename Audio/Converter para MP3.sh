738ba4053d5517b9712378bfc8a2255905d49a41
Mateus Ferreira <huh.mateus@outlook.com>
2025-10-26 13:09:44 -0300

    Incluido comentarios sobre os blocos de codigo.

--- Audio/Converter para MP3.sh
+++ Audio/Converter para MP3.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-{
 readarray FILENAME <<< "$(echo -e "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" | sed -e 's/\r//g')"
 
 for file in "${FILENAME[@]}"; do
@@ -15,19 +14,18 @@ for file in "${FILENAME[@]}"; do
             if [[ $line =~ time=([0-9:.]+) ]]; then
                 current_time=$(echo "${BASH_REMATCH[1]}" | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
                 progress=$(( (current_time * 100) / duration ))
-                echo "$progress"
+                echo "$progress" #Captura a duração da converção
             fi
         done | zenity --progress \
             --title="Convertendo Áudio" \
             --text="Convertendo $file..." \
             --percentage=0 \
-            --auto-close
+            --auto-close #Informa que a conversão ocorreu com sucesso
     else
-        notify-send "Erro" "O arquivo $file não é um áudio." --app-name="Conversor"
-        exit
+        # Informa que o arquivo fornecido não e um audio.
+        zenity --error \
+            --title="Conversor" \
+            --text="O arquivo $file não é um áudio."
     fi
+
 done
-
-notify-send "Deu certo!" "Os arquivos foram convertidos para MP3." --app-name="Conversor"
-}
-
